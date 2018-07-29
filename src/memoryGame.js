60b7a8e2457f12d09949800bb615a5db5ad34530
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-07-29 14:41:51 -0700

    create memoryGame component

---
+++ src/memoryGame.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MemoryGame extends Component {
+	render() {
+		return (
+			<div className="projects container">
+				
+			</div>
+		);
+	}
+}
+
+export default MemoryGame;
