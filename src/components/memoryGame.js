e11dbb1021de69dc3332fadb4c877ddeb7f38ad2
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-07-30 10:16:35 -0700

    import Link, link to component path

--- src/components/memoryGame.js
+++ src/components/memoryGame.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class MemoryGame extends Component {
 	render() {
 		return (
 			<div>
-				<img src="http://www.allwhitebackground.com/images/2/2581.jpg" alt="Memory Game"/>
-				<p><h4>Memory Game</h4></p>
+				<Link to='/projects/memoryGame'>
+					
+				</Link>
 			</div>
 		);
 	}
