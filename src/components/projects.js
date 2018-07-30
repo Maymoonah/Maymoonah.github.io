7db1051886f58cc56cfb16f42ac60248216434c1
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-07-30 10:17:57 -0700

    remove child components, add images and headings to projects divs

--- src/components/projects.js
+++ src/components/projects.js
@@ -8,20 +8,24 @@ class Projects extends Component {
 	render() {
 		return (
 			<div className="container">
-				<h1>Projects</h1>
+				<h1 className="title">Projects</h1>
 				<div className="projects">
 					<div className="row">
 						<div className="col-md-3 project">
-							<MemoryGame />
+							<img src="http://www.allwhitebackground.com/images/2/2581.jpg" alt="Memory Game"/>
+							<h4>Memory Game</h4>
 						</div>
 						<div className="col-md-3 project">
-							<ArcadeGame />
+							<img src="http://www.allwhitebackground.com/images/2/2581.jpg" alt="Arcade Game"/>
+							<h4>Arcade Game</h4>
 						</div>
 						<div className="col-md-3 project">
-							<PixelArt />
+							<img src="http://www.allwhitebackground.com/images/2/2581.jpg" alt="Pixel Art"/>
+							<h4>Pixel Art</h4>
 						</div>
 						<div className="col-md-3 project">
-							<NeighborhoodMap />
+							<img src="http://www.allwhitebackground.com/images/2/2581.jpg" alt="Neighborhood Map"/>
+							<h4>Neighborhood Map</h4>
 						</div>
 					</div>
 				</div>
