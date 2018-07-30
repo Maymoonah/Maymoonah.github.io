7b4e4aecbc922e6a648ea7414d7b083dbfb38f60
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-07-30 11:16:09 -0700

    add image and projecct description and wrap elements with bootstrap

--- src/components/memoryGame.js
+++ src/components/memoryGame.js
@@ -8,7 +8,18 @@ class MemoryGame extends Component {
 				<Link to='/projects'>
 					<h1 className="title">Projects</h1>
 				</Link>
-				MemoryGame
+				<div className="row">
+					<div className="col-md-6">
+						<img src="http://www.allwhitebackground.com/images/2/2581.jpg" alt="Memory Game" width="200px" height="200px"/>
+					</div>
+					<div className="col-md-6">
+						<p>
+							- Flip over pairs of matching cards with static non-interactive starter code provided by Udacity.<br />
+							- I Added game functionality such as click handling, matching logic, and winning condition using JavaScript and jQuery.<br />
+							- A player can flip no more than two cards at a time until all cards are matched.
+						</p>
+					</div>
+				</div>
 			</div>
 		);
 	}
