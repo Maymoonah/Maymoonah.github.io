89cffe5484ff891b24d363b399404974aa207d4c
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-07-30 10:28:08 -0700

    move link to projectects component, add link to main projects page

--- src/components/memoryGame.js
+++ src/components/memoryGame.js
@@ -5,9 +5,10 @@ class MemoryGame extends Component {
 	render() {
 		return (
 			<div>
-				<Link to='/projects/memoryGame'>
-					
+				<Link to='/projects'>
+					<h1 className="title">Projects</h1>
 				</Link>
+				MemoryGame
 			</div>
 		);
 	}
