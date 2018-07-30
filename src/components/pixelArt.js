08572e12a4f0a82cbc259f3106223cc4d0094a4f
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-07-30 11:16:18 -0700

    add image and projecct description and wrap elements with bootstrap

--- src/components/pixelArt.js
+++ src/components/pixelArt.js
@@ -8,7 +8,18 @@ class PixelArt extends Component {
 				<Link to='/projects'>
 					<h1 className="title">Projects</h1>
 				</Link>
-				PixelArt
+				<div className="row">
+					<div className="col-md-6">
+						<img src="http://www.allwhitebackground.com/images/2/2581.jpg" alt="Pixel Art" width="200px" height="200px"/>
+					</div>
+					<div className="col-md-6">
+						<p>
+							- Customizable canvas where the user can create art by coloring pixels.
+							<br />
+							- I created the application using HTML5, CSS3, and JavaScript.
+						</p>
+					</div>
+				</div>
 			</div>
 		);
 	}
