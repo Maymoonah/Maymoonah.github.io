00d732f6ffc6cd18324380d4f5f1942ba1e9e87e
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-07-30 10:29:11 -0700

    link projects to projects component pages

--- src/components/projects.js
+++ src/components/projects.js
@@ -3,6 +3,7 @@ import MemoryGame from './memoryGame';
 import ArcadeGame from './arcadeGame';
 import PixelArt from './pixelArt';
 import NeighborhoodMap from './neighborhoodMap';
+import { Link } from 'react-router-dom';
 
 class Projects extends Component {
 	render() {
@@ -12,20 +13,28 @@ class Projects extends Component {
 				<div className="projects">
 					<div className="row">
 						<div className="col-md-3 project">
-							<img src="http://www.allwhitebackground.com/images/2/2581.jpg" alt="Memory Game"/>
-							<h4>Memory Game</h4>
+							<Link to='/projects/memoryGame'>
+								<img src="http://www.allwhitebackground.com/images/2/2581.jpg" alt="Memory Game"/>
+								<h4>Memory Game</h4>
+							</Link>
 						</div>
 						<div className="col-md-3 project">
-							<img src="http://www.allwhitebackground.com/images/2/2581.jpg" alt="Arcade Game"/>
-							<h4>Arcade Game</h4>
+							<Link to='/projects/arcadeGame'>
+								<img src="http://www.allwhitebackground.com/images/2/2581.jpg" alt="Arcade Game"/>
+								<h4>Arcade Game</h4>
+							</Link>
 						</div>
 						<div className="col-md-3 project">
-							<img src="http://www.allwhitebackground.com/images/2/2581.jpg" alt="Pixel Art"/>
-							<h4>Pixel Art</h4>
+							<Link to='/projects/pixelArt'>
+								<img src="http://www.allwhitebackground.com/images/2/2581.jpg" alt="Pixel Art"/>
+								<h4>Pixel Art</h4>
+							</Link>
 						</div>
 						<div className="col-md-3 project">
-							<img src="http://www.allwhitebackground.com/images/2/2581.jpg" alt="Neighborhood Map"/>
-							<h4>Neighborhood Map</h4>
+							<Link to='/projects/neighborhoodMap'>
+								<img src="http://www.allwhitebackground.com/images/2/2581.jpg" alt="Neighborhood Map"/>
+								<h4>Neighborhood Map</h4>
+							</Link>
 						</div>
 					</div>
 				</div>
