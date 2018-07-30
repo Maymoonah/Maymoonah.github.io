8d6691aef2ecb46180d22a9c57c0b8d4e85f44bc
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-07-30 11:16:29 -0700

    add image and projecct description and wrap elements with bootstrap

--- src/components/neighborhoodMap.js
+++ src/components/neighborhoodMap.js
@@ -8,7 +8,18 @@ class NeighborhoodMap extends Component {
 				<Link to='/projects'>
 					<h1 className="title">Projects</h1>
 				</Link>
-				NeighborhoodMap
+				<div className="row">
+					<div className="col-md-6">
+						<img src="http://www.allwhitebackground.com/images/2/2581.jpg" alt="Neighborhood Map" width="200px" height="200px"/>
+					</div>
+					<div className="col-md-6">
+						<p>
+							- Map of places in my neighborhood such as restaurants and schools.<br/>
+							- Created a map using Google Maps JavaScript API and the framework Knockout.js.<br/>
+							- The user can read additional information and see a picture of the location.
+						</p>
+					</div>
+				</div>
 			</div>
 		);
 	}
