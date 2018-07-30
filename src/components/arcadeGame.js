a33d835c3adc6a465d3eb89d24509f44862f141c
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-07-30 11:15:53 -0700

    add image and projecct description and wrap elements with bootstrap

--- src/components/arcadeGame.js
+++ src/components/arcadeGame.js
@@ -8,7 +8,18 @@ class ArcadeGame extends Component {
 				<Link to='/projects'>
 					<h1 className="title">Projects</h1>
 				</Link>
-				ArcadeGame
+				<div className="row">
+					<div className="col-md-6">
+						<img src="http://www.allwhitebackground.com/images/2/2581.jpg" alt="Arcade Game" width="200px" height="200px"/>
+					</div>
+					<div className="col-md-6">
+						<p>
+							- Recreating the classic arcade game Frogger using Art assets and game engine provided by Udacity.<br/>
+							- Implemented Player, enemy, and other entities using Object-Oriented JavaScript.<br/>
+							- Player and multiple enemies which can move around have been instantiated.
+						</p>
+					</div>
+				</div>
 			</div>			
 		);
 	}
