eab1aed4776be87ddf10ed42076624a3cfd138f1
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-11-13 12:57:27 -0800

    edit navbar className

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 class Navbar extends Component {
 	render() {
 		return (
-			<nav className="navbar navbar-expand-lg navigation">
+			<nav className="navbar navbar-expand-lg navbar-light bg-light navigation">
 			  <Link className="navbar-brand" to='/'>Maymoonah Mohammed</Link>
 			  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
 			    <span className="navbar-toggler-icon"></span>
 			  </button>
 			  <div className="collapse navbar-collapse" id="navbarNavDropdown">
 			    <ul className="navbar-nav">
-			      <li className="nav-item active">
+			      <li className="nav-item">
 			        <Link className="nav-link" to='/'>About Me</Link>
 			      </li>
 			      <li className="nav-item">
