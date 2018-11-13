52e4f425ff6dd0fd73656e4e3c278cf9fc064e0e
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-11-13 12:30:13 -0800

    add bootstrap navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,14 +4,28 @@ import { Link } from 'react-router-dom';
 class Navbar extends Component {
 	render() {
 		return (
-			<nav className="navbar navbar-dark bg-dark">
-				<Link to='/projects'>
-					<h2>Projects</h2>
-				</Link>
-				<Link to='/'>
-					<h2>Home</h2>
-				</Link>
-			</nav>	
+			<nav className="navbar navbar-expand-lg navigation">
+			  <Link className="navbar-brand" to='/'>Maymoonah Mohammed</Link>
+			  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+			    <span className="navbar-toggler-icon"></span>
+			  </button>
+			  <div className="collapse navbar-collapse" id="navbarNavDropdown">
+			    <ul className="navbar-nav">
+			      <li className="nav-item active">
+			        <Link className="nav-link" to='/'>About Me</Link>
+			      </li>
+			      <li className="nav-item">
+			        <Link className="nav-link" to='/'>Projects</Link>
+			      </li>
+			      <li className="nav-item">
+			        <Link className="nav-link" to='/'>Resume</Link>
+			      </li>
+			      <li className="nav-item">
+			        <Link className="nav-link" to='/'>Contact</Link>
+			      </li>
+			    </ul>
+			  </div>
+			</nav>
 		);
 	}
 }
