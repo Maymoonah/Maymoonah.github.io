1007eab72ac4127c39d84e24c21bdbc3838fd6f5
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-11-14 11:51:49 -0800

    create resume component

---
+++ src/components/resume.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from './navbar';
+
+class resume extends Component {
+	render() {
+		return (
+			<div>
+				<Navbar />		
+				
+			</div>
+		);
+	}
+}
+
+export default aboutMe;
