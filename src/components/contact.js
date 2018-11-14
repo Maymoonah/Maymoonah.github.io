3431787070ec4c1e47b7f6abaa419eafa56aeb48
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-11-14 11:51:21 -0800

    create contact component

---
+++ src/components/contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from './navbar';
+
+class contact extends Component {
+	render() {
+		return (
+			<div>
+				<Navbar />		
+				
+			</div>
+		);
+	}
+}
+
+export default contact;
