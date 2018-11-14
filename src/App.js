aabc1870053a71f887c233f7c54e4d96a7f9dfd6
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-11-14 11:37:01 -0800

    edit jumbotron

--- src/App.js
+++ src/App.js
@@ -7,19 +7,22 @@ import MemoryGame from './components/memoryGame';
 import ArcadeGame from './components/arcadeGame';
 import NeighborhoodMap from './components/neighborhoodMap';
 import PixelArt from './components/pixelArt';
+import Navbar from './components/navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="main">
+        <Navbar />
         <Route exact path="/" render={() => (
-          <div className="jumbotron">
-            <h1 className="display-4">Welcome to Maymoonah's Portfolio!</h1>
-            <hr className="my-4" />
-            <p className="lead">
-                <Link to="/projects" className="btn btn-primary btn-lg" role="button">Check Out My Projects!</Link>
-            </p>
-          </div>
+          // <div className="jumbotron">
+          //   <h1 className="display-4">Welcome to Maymoonah's Portfolio!</h1>
+          //   <hr className="my-4" />
+          //   <p className="lead">
+          //       <Link to="/projects" className="btn btn-primary btn-lg" role="button">Check Out My Projects!</Link>
+          //   </p>
+          // </div>
+          <img src="May.JPG" alt="May"/>
         )} />
         <Route exact path="/projects" component={Projects} />
         <Route exact path="/projects/memoryGame" component={MemoryGame} />
