72d09913cb7623e29450103f5ab1afa832c73123
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-11-15 11:33:10 -0800

    Add heading

--- src/components/resume.js
+++ src/components/resume.js
@@ -4,8 +4,12 @@ import { Link } from 'react-router-dom';
 class Resume extends Component {
 	render() {
 		return (
-			<div>		
-				Resume
+			<div className="resume container">		
+				<div className="row">
+					<div className="col-md-12">
+						<h1 className="title">Resume</h1>
+					</div>
+				</div>
 			</div>
 		);
 	}
