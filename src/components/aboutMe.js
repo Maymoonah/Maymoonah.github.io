198c18f63abae9ff61c4a68bfb97461e4887fd32
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-11-15 11:31:15 -0800

    Add heading

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -4,9 +4,25 @@ import { Link } from 'react-router-dom';
 class AboutMe extends Component {
 	render() {
 		return (
-			<div>	
-				<h1>About Me</h1>
-				<img src="../images/May.JPG" alt="Maymoonah"/>
+			<div className="about container">
+				<div className="row">
+					<div className="col-md-12">
+						<h1 className="title">About Me</h1>
+					</div>
+				</div>
+
+				<div className="row">
+					<div className="col-md-4">
+						<img src="../../images/May.JPG" alt="Maymoonah"/>
+					</div>
+					<div className="col-md-8">
+						<p className="description">
+							Front-End Web Developer skilled in building web applications using HTML, CSS, and JavaScript.
+							Over 7 years of experience in education which taught me patience in dealing with different 
+							personality types and problem-solving by formulating solutions to different learning hindrances.
+						</p>
+					</div>
+				</div>
 			</div>
 		);
 	}
