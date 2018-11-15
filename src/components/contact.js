c0c2fb98d277e38335afd48db6f5ad6bd967e833
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-11-15 11:31:36 -0800

    Add heading

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,8 +4,12 @@ import { Link } from 'react-router-dom';
 class Contact extends Component {
 	render() {
 		return (
-			<div>		
-				Contact
+			<div className="contact container">		
+				<div className="row">
+					<div className="col-md-12">
+						<h1 className="title">Contact</h1>
+					</div>
+				</div>
 			</div>
 		);
 	}
