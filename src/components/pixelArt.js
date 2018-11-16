a34968b3ca2e19c466fb0dc626f2998a865accb7
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-11-15 19:43:15 -0800

    add navbar component

--- src/components/pixelArt.js
+++ src/components/pixelArt.js
@@ -6,6 +6,7 @@ class PixelArt extends Component {
 	render() {
 		return (
 			<div>
+				<Navbar />
 				<div className="row">
 					<div className="col-md-4">
 						<img className="images" src="http://www.allwhitebackground.com/images/2/2581.jpg" alt="Pixel Art" width="100%" height="100%"/>
@@ -17,9 +18,9 @@ class PixelArt extends Component {
 							- I created the application using HTML5, CSS3, and JavaScript.
 						</p>
 						<br/>
-						<a href='https://maymoonah.github.io/Pixel-Art-Maker/'>
+						<Link to='https://maymoonah.github.io/Pixel-Art-Maker/'>
 							<p>Go To Pixel Art!</p>
-						</a>
+						</Link>
 					</div>
 				</div>
 			</div>
