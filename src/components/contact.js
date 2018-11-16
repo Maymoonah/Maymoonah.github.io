8456413947552f73f3851127d95e4a6a57c96bc7
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-11-15 21:32:30 -0800

    remove navbar from component

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
-import Navbar from './navbar';
 
 class Contact extends Component {
 	render() {
 		return (
-			<div>
-				<Navbar />
-				<div className="contact container">		
-					<div className="row">
-						<div className="col-md-12">
-							<h1 className="title">Contact</h1>
-						</div>
+			<div className="contact container">		
+				<div className="row">
+					<div className="col-md-12">
+						<h1 className="title">Contact</h1>
 					</div>
 				</div>
 			</div>
