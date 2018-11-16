c66ca16eb8c0de3ba903c95ba3f93cd09290683c
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-11-15 21:32:44 -0800

    add navbar component

--- src/App.js
+++ src/App.js
@@ -10,12 +10,13 @@ import AboutMe from './components/aboutMe';
 import Resume from './components/resume';
 import Contact from './components/contact';
 import Home from './components/home';
+import Navbar from './components/navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="main">
-        
+        <Navbar />
         <Route exact path="/" component={Home} />
         <Route path="/aboutme" component={AboutMe} />
         <Route path="/resume" component={Resume} />
