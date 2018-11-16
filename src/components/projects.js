20400de07ac5cba9ab6dbc270cd9cf28688408cb
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-11-16 11:17:19 -0800

    change bootstrap grid layout for projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -12,25 +12,27 @@ class Projects extends Component {
 				</div>
 				<div className="projects">
 					<div className="row">
-						<div className="col-md-3 project">
+						<div className="col-md-6 project">
 							<Link to='/projects/memorygame'>
-								<img src="../images/Memory.png" alt="Memory Game"/>
+								<img src="http://www.allwhitebackground.com/images/2/2581.jpg" alt="Memory Game"/>
 								<h4>Memory Game</h4>
 							</Link>
 						</div>
-						<div className="col-md-3 project">
+						<div className="col-md-6 project">
 							<Link to='/projects/arcadegame'>
 								<img src="http://www.allwhitebackground.com/images/2/2581.jpg" alt="Arcade Game"/>
 								<h4>Arcade Game</h4>
 							</Link>
 						</div>
-						<div className="col-md-3 project">
+					</div>
+					<div className="row">
+						<div className="col-md-6 project">
 							<Link to='/projects/pixelart'>
 								<img src="http://www.allwhitebackground.com/images/2/2581.jpg" alt="Pixel Art"/>
 								<h4>Pixel Art</h4>
 							</Link>
 						</div>
-						<div className="col-md-3 project">
+						<div className="col-md-6 project">
 							<Link to='/projects/neighborhoodmap'>
 								<img src="http://www.allwhitebackground.com/images/2/2581.jpg" alt="Neighborhood Map"/>
 								<h4>Neighborhood Map</h4>
