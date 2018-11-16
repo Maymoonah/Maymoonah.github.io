c30db11d97fca1b067781b5d28a80611e0705e78
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-11-15 21:32:14 -0800

    remove navbar from component

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
-import Navbar from './navbar';
 
 class Resume extends Component {
 	render() {
 		return (
-			<div>
-				<Navbar />
-				<div className="resume container">	
-					<div className="row">
-						<div className="col-md-12">
-							<h1 className="title">Resume</h1>
-						</div>
+			<div className="resume container">	
+				<div className="row">
+					<div className="col-md-12">
+						<h1 className="title">Resume</h1>
 					</div>
 				</div>
 			</div>
