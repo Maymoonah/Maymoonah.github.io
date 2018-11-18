82f6bef798a43a14b190eb17b2ca5a8ffa1344eb
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-11-17 23:48:44 -0800

    add container and map classes to component

--- src/components/neighborhoodMap.js
+++ src/components/neighborhoodMap.js
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 class NeighborhoodMap extends Component {
 	render() {
 		return (
-			<div>
+			<div className="container map">
 				<div className="row">
 					<div className="col-md-4">
 						<img className="images" src="http://www.allwhitebackground.com/images/2/2581.jpg" alt="Neighborhood Map" width="100%" height="100%"/>
 					</div>
-					<div className="col-md-8">
-						<p className="description">
+					<div className="col-md-8 description">
+						<p>
 							- Map of places in my neighborhood such as restaurants and schools.<br/>
 							- Created a map using Google Maps JavaScript API and the framework Knockout.js.<br/>
 							- The user can read additional information and see a picture of the location.
