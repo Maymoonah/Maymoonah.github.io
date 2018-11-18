935ba33d1bae96ef707ad66f4b0bcc500bf70453
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-11-17 23:48:29 -0800

    add container and memory classes to component

--- src/components/memoryGame.js
+++ src/components/memoryGame.js
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 class MemoryGame extends Component {
 	render() {
 		return (
-			<div>
+			<div className="container memory">
 				<div className="row">
 					<div className="col-md-4">
-						<img src="~" alt="Memory Game" width="100%" height="100%"/>
+						<img src="../../images/Memory.png" alt="Memory Game" width="100%" height="100%"/>
 					</div>
-					<div className="col-md-8">
-						<p className="description">
+					<div className="col-md-8 description">
+						<p>
 							- Flip over pairs of matching cards with static non-interactive starter code provided by Udacity.<br />
 							- I Added game functionality such as click handling, matching logic, and winning condition using JavaScript and jQuery.<br />
 							- A player can flip no more than two cards at a time until all cards are matched.
