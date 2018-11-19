e1cd938e46f9275326ef76c34e2611d211877068
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-11-18 22:41:54 -0800

    create contact form

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 
+
 class Contact extends Component {
 	render() {
 		return (
 			<div className="container contact">		
 				<div className="row">
 					<div className="col-md-12">
-						<h1 className="title">Contact</h1>
+						<h1 className="title">Contact Me</h1>
 					</div>
 				</div>
+				<form>
+					<input type="text" placeholder="First Name" required />
+					<input type="text" placeholder="Last Name" required />
+					<input type="email" placeholder="Email Address" required />
+					<textarea placeholder="Message" required></textarea>					
+				</form>
 			</div>
 		);
 	}
