940bd15aa9092f57b30836f70be21c4d61aba948
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-11-18 22:42:38 -0800

    import Navbar..etc

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+// import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 class Navbar extends Component {
 	render() {
