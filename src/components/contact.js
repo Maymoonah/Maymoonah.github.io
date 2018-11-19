9f757d814e876425ffd3acb40695d5594d1d9446
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-11-19 09:57:49 -0800

    import react-bootstrap elements, add form controls

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,23 +1,18 @@
 import React, { Component } from 'react';
-
+import { form, FormGroup, FormControl, Button } from 'react-bootstrap';
 
 class Contact extends Component {
 	render() {
 		return (
-			<div className="container contact">		
-				<div className="row">
-					<div className="col-md-12">
-						<h1 className="title">Contact Me</h1>
-					</div>
-				</div>
-				<form action="contactForm.php" method="post">
-					<input type="text" placeholder="First Name" required />
-					<input type="text" placeholder="Last Name" required />
-					<input type="email" placeholder="Email Address" required />
-					<textarea placeholder="Message" required></textarea>	
-					<input type="submit" value="Send"/>									
-				</form>
-			</div>
+			<form className="contact-form" onSubmit={this.gotEmail}>
+			  <FormGroup role="form">
+			    <FormControl type="text" className="form-control" name="firstName"/>
+			    <FormControl type="text" className="form-control" name="lastName"/>
+			    <FormControl type="text" className="form-control" name="email"/>
+			    <FormControl className="form-control" componentClass="textarea" placeholder="Message..." />
+			    <Button className="btn btn-primary btn-large centerButton" type="submit">Send</Button>
+			  </FormGroup>
+			</form>
 		);
 	}
 }
