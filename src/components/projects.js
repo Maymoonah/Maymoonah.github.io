de64c3667f2ae5c345c4999f705c8ab29797f994
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-11-25 22:34:31 -0800

    put each project in different col-md-12 rows

--- src/components/projects.js
+++ src/components/projects.js
@@ -12,13 +12,15 @@ class Projects extends Component {
 				</div>
 				<div className="projects">
 					<div className="row">
-						<div className="col-md-6 project">
+						<div className="col-md-12 project">
 							<Link to='/projects/memorygame'>
 								<img src="../images/Memory.jpg" alt="Memory Game"/>
 								<h4>Memory Game</h4>
 							</Link>
 						</div>
-						<div className="col-md-6 project">
+					</div>
+					<div className="row">
+						<div className="col-md-12 project">
 							<Link to='/projects/arcadegame'>
 								<img src="../images/Arcade.png" alt="Arcade Game"/>
 								<h4>Arcade Game</h4>
@@ -26,13 +28,15 @@ class Projects extends Component {
 						</div>
 					</div>
 					<div className="row">
-						<div className="col-md-6 project">
+						<div className="col-md-12 project">
 							<Link to='/projects/pixelart'>
 								<img src="../images/Pixel.png" alt="Pixel Art"/>
 								<h4>Pixel Art</h4>
 							</Link>
 						</div>
-						<div className="col-md-6 project">
+					</div>
+					<div className="row">
+						<div className="col-md-12 project">
 							<Link to='/projects/neighborhoodmap'>
 								<img src="../images/Map.png" alt="Neighborhood Map"/>
 								<h4>Neighborhood Map</h4>
