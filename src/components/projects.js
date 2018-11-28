4d4539f4bf535651376e4a2adf1bf3c7221d32ff
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-11-28 08:34:56 -0800

    edit projects bootstrap, add class 'link' to all Link tags

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import memory from '../images/Memory.jpg';
+import arcade from '../images/Arcade.jpg';
+import pixel from '../images/Pixel.png';
+import map from '../images/Map.jpg';
 
 class Projects extends Component {
 	render() {
@@ -12,33 +16,31 @@ class Projects extends Component {
 				</div>
 				<div className="projects">
 					<div className="row">
-						<div className="col-md-12 project">
-							<Link to='/projects/memorygame'>
-								<img src="../images/Memory.jpg" alt="Memory Game"/>
+						<div className="col-md-6 project">
+							<Link to='/projects/memorygame' class="link">
+								<img src={memory} alt="Memory Game"/>
 								<h4>Memory Game</h4>
 							</Link>
 						</div>
-					</div>
-					<div className="row">
-						<div className="col-md-12 project">
-							<Link to='/projects/arcadegame'>
-								<img src="../images/Arcade.png" alt="Arcade Game"/>
+					
+						<div className="col-md-6 project">
+							<Link to='/projects/arcadegame' class="link">
+								<img src={arcade} alt="Arcade Game"/>
 								<h4>Arcade Game</h4>
 							</Link>
 						</div>
 					</div>
 					<div className="row">
-						<div className="col-md-12 project">
-							<Link to='/projects/pixelart'>
-								<img src="../images/Pixel.png" alt="Pixel Art"/>
+						<div className="col-md-6 project">
+							<Link to='/projects/pixelart' class="link">
+								<img src={pixel} alt="Pixel Art"/>
 								<h4>Pixel Art</h4>
 							</Link>
 						</div>
-					</div>
-					<div className="row">
-						<div className="col-md-12 project">
-							<Link to='/projects/neighborhoodmap'>
-								<img src="../images/Map.png" alt="Neighborhood Map"/>
+					
+						<div className="col-md-6 project">
+							<Link to='/projects/neighborhoodmap' class="link">
+								<img src={map} alt="Neighborhood Map"/>
 								<h4>Neighborhood Map</h4>
 							</Link>
 						</div>
