6a7f93270af9c23354a021cd90f894e7f059e6d6
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-11-28 09:03:15 -0800

    edit bootsrap col

--- src/components/projects.js
+++ src/components/projects.js
@@ -16,13 +16,13 @@ class Projects extends Component {
 				</div>
 				<div className="projects">
 					<div className="row">
-						<div className="col-md-6 project">
+						<div className="col-md-5 project">
 							<Link to='/projects/memorygame' class="link">
 								<img src={memory} alt="Memory Game"/>
 								<h4>Memory Game</h4>
 							</Link>
 						</div>
-						<div className="col-md-6 project">
+						<div className="col-md-5 project">
 							<Link to='/projects/arcadegame' class="link">
 								<img src={arcade} alt="Arcade Game"/>
 								<h4>Arcade Game</h4>
@@ -30,13 +30,13 @@ class Projects extends Component {
 						</div>
 					</div>
 					<div className="row">
-						<div className="col-md-6 project">
+						<div className="col-md-5 project">
 							<Link to='/projects/pixelart' class="link">
 								<img src={pixel} alt="Pixel Art"/>
 								<h4>Pixel Art</h4>
 							</Link>
 						</div>
-						<div className="col-md-6 project">
+						<div className="col-md-5 project">
 							<Link to='/projects/neighborhoodmap' class="link">
 								<img src={map} alt="Neighborhood Map"/>
 								<h4>Neighborhood Map</h4>
