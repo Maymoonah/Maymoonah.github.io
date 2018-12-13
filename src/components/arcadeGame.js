5e1867e7d0282c4347275054cc393678a5d676d1
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-12-13 10:05:57 -0800

    edit project description

--- src/components/arcadeGame.js
+++ src/components/arcadeGame.js
@@ -14,7 +14,14 @@ class ArcadeGame extends Component {
 						<ul>
 							<li>Recreating the classic arcade game Frogger using Art assets and game engine provided by Udacity.</li><br/>
 							<li>Implemented Player, enemy, and other entities using Object-Oriented JavaScript.</li><br/>
-							<li>Player and multiple enemies which can move around have been instantiated.</li>
+							<li>Player and multiple enemies which can move around have been instantiated.</li><br />
+							<li>This game has 3 levels and 5 lives which reset at each level. The player needs 5 points to move on to the next level.
+							Each time the player crosses the field safely to the water, he scores 1 point.</li><br />
+							<li>The objective of the game is to get the player across the field to the water without running into the enemy bugs.</li><br />
+							<li>The player can collect different types of gems along the way.</li><br />
+							<li>Once the player crosses to the river 5 times without losing all lives, the player moves to the next level where the speed of 
+							the enemy bugs increases.</li><br />
+							<li>The game ends once the player successfully completes all 3 levels, or when the player loses all lives.</li>
 						</ul>
 						<br/>
 							<button className="btn projectBtn"><a href="https://maymoonah.github.io/Arcade-Game/">Go To Arcade Game!</a></button>
